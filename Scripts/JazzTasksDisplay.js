7838d824ff648cc13294aa591e326795bf7ea013
Gunnar Liden <gunnar.liden@jazzliveaarau.ch>
2021-04-19 19:31:57 +0200

    Display active record half-made

--- Scripts/JazzTasksDisplay.js
+++ Scripts/JazzTasksDisplay.js
@@ -44,7 +44,7 @@ function initJazzTasksDisplayAfterLoadOfXml()
 {
     g_display_table = new JazzTasksTable(g_display_xml);
 
-    g_display_number = 4;
+    g_display_number = 1;
 
     g_active_record = g_display_table.getJazzTaskRecord(g_display_number);
 
@@ -67,6 +67,47 @@ function onClickCloseActiveRecord()
 
 } // onClickCloseActiveRecord
 
+// Download the DOC document
+function onClickDownloadDoc()
+{
+    alert("onClickDownloadDoc Enter");
+
+} // onClickDownloadDoc
+
+// Download the DOC document
+function onClickDownloadPdf()
+{
+    alert("onClickDownloadPdf Enter");
+
+} // onClickDownloadPdf
+
+// Open reference one URL
+function onClickOpenUrlOne()
+{
+    alert("onClickOpenUrlOne Enter");
+
+} // onClickOpenUrlOne
+
+// Open reference two URL
+function onClickOpenUrlTwo()
+{
+    alert("onClickOpenUrlTwo Enter");
+
+} // onClickOpenUrlTwo
+
+// Open reference three URL
+function onClickOpenUrlThree()
+{
+    alert("onClickOpenUrlThree Enter");
+
+} // onClickOpenUrlThree
+
+// Open reference four URL
+function onClickOpenUrlFour()
+{
+    alert("onClickOpenUrlFour Enter");
+
+} // onClickOpenUrlFour
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// End Event Functions /////////////////////////////////////////////
@@ -81,9 +122,20 @@ function setActiveRecordDiv()
 {
     setActiveRecordDivTitle();
 
+    setActiveRecordDivNumber();
+
+    setActiveRecordDivDescription();
+
+    setActiveRecordDivRemark();
+
+    setActiveRecordDivDocument();
+
+    setActiveRecordDivReferenceOneUrl();
+
+    setActiveRecordDivReferenceOneDescription();
+
 } // setActiveRecordDiv
 
-// getDivElementActiveRecordTitle()
 // Sets the text of the active record title <div> element
 function setActiveRecordDivTitle()
 {
@@ -98,6 +150,113 @@ function setActiveRecordDivTitle()
     el_div_title.innerHTML = title_str;
 
 } // setActiveRecordDivTitle
+
+// Sets the text of the active record number <div> element
+function setActiveRecordDivNumber()
+{
+    var el_div_number = getDivElementActiveRecordNumber();
+
+    var number_str = '';
+
+    number_str = number_str + '<b>' + 'Nummer' + '</b>' + '<br>';
+
+    number_str = number_str + g_active_record.getJazzTaskRegNumber();
+
+    el_div_number.innerHTML = number_str;
+
+} // setActiveRecordDivNumber
+
+// Sets the text of the active record description <div> element
+function setActiveRecordDivDescription()
+{
+    var el_div_description = getDivElementActiveRecordDescription();
+
+    var description_str = '';
+
+    description_str = description_str + '<b>' + 'Beschreibung/Suchwörter' + '</b>' + '<br>';
+
+    description_str = description_str + g_active_record.getJazzTaskDescription();
+
+    el_div_description.innerHTML = description_str;
+
+} // setActiveRecordDivDescription
+
+// Sets the text of the active record remark <div> element
+function setActiveRecordDivRemark()
+{
+    var el_div_remark = getDivElementActiveRecordRemark();
+
+    var remark_str = '';
+
+    remark_str = remark_str + '<b>' + 'Bemerkung' + '</b>' + '<br>';
+
+    remark_str = remark_str + g_active_record.getJazzTaskRemark();
+
+    el_div_remark.innerHTML = remark_str;
+
+} // setActiveRecordDivRemark
+
+// Sets the text of the active record document <div> element
+function setActiveRecordDivDocument()
+{
+    var el_div_document_name = getDivElementActiveRecordDocumentName();
+
+    var document_name_str = '';
+
+    document_name_str = document_name_str + '<b>' + 'Dokument ' + '</b>';
+
+    document_name_str = document_name_str + g_active_record.getJazzTaskRegNumber();
+
+    el_div_document_name.innerHTML = document_name_str;
+
+} // setActiveRecordDivDocument
+
+// Sets the text of the active record reference one URL <div> element
+function setActiveRecordDivReferenceOneUrl()
+{
+    var el_div_ref_one_url = getDivElementActiveRecordReferenceOneUrl();
+	
+	var reference_number = 1;
+
+    var url_str = g_active_record.getJazzTaskRefLink(reference_number);
+
+    if (url_str.length == 0)
+    {
+        hideActiveRecordReferenceOne();
+
+        return;
+    }
+    else
+    {
+        displayActiveRecordReferenceOne();
+    }
+
+    var ref_one_url_str = '';
+
+    ref_one_url_str = ref_one_url_str + '<b>' + 'Referenz ' + reference_number.toString() + '</b>' + '<br>';
+
+    ref_one_url_str = ref_one_url_str + url_str;
+
+    el_div_ref_one_url.innerHTML = ref_one_url_str;
+
+} // setActiveRecordDivReferenceOneUrl
+
+// Sets the text of the active record reference one description <div> element
+function setActiveRecordDivReferenceOneDescription()
+{
+    var el_div_ref_one_description = getDivElementActiveRecordReferenceOneDescription();
+	
+	var reference_number = 1;
+
+    var description_str = g_active_record.getJazzTaskRefDescription(reference_number);
+
+    var ref_one_description_str = '';
+
+    ref_one_description_str = ref_one_description_str + description_str;
+
+    el_div_ref_one_description.innerHTML = ref_one_description_str;
+
+} // setActiveRecordDivReferenceOneDescription
 
 
 
@@ -154,6 +313,124 @@ function getIdDivElementActiveRecordTitle()
     return 'id_active_record_title';
 
 } // getIdDivElementActiveRecordTitle
+
+// Returns the active record number <div> element
+function getDivElementActiveRecordNumber()
+{
+    return document.getElementById(getIdDivElementActiveRecordNumber());
+
+} // getDivElementActiveRecordNumber
+
+//Returns the identity of the active record number <div> element
+function getIdDivElementActiveRecordNumber()
+{
+    return 'id_active_record_number';
+
+} // getIdDivElementActiveRecordNumber
+
+// Returns the active record description <div> element
+function getDivElementActiveRecordDescription()
+{
+    return document.getElementById(getIdDivElementActiveRecordDescription());
+
+} // getDivElementActiveRecordDescription
+
+//Returns the identity of the active record description <div> element
+function getIdDivElementActiveRecordDescription()
+{
+    return 'id_active_record_description';
+
+} // getIdDivElementActiveRecordDescription
+
+// Returns the active record remark <div> element
+function getDivElementActiveRecordRemark()
+{
+    return document.getElementById(getIdDivElementActiveRecordRemark());
+
+} // getDivElementActiveRecordRemark
+
+//Returns the identity of the active record remark <div> element
+function getIdDivElementActiveRecordRemark()
+{
+    return 'id_active_record_remark';
+
+} // getIdDivElementActiveRecordRemark
+
+// Returns the active record document name <div> element
+function getDivElementActiveRecordDocumentName()
+{
+    return document.getElementById(getIdDivElementActiveRecordDocumentName());
+
+} // getDivElementActiveRecordDocumentName
+
+//Returns the identity of the active record document name <div> element
+function getIdDivElementActiveRecordDocumentName()
+{
+    return 'id_active_record_document_name';
+
+} // getIdDivElementActiveRecordDocumentName
+
+
+// Returns the active record reference one <div> element
+function getDivElementActiveRecordReferenceOne()
+{
+    return document.getElementById(getIdDivElementActiveRecordReferenceOne());
+
+} // getDivElementActiveRecordReferenceOne
+
+//Returns the identity of the active record reference one <div> element
+function getIdDivElementActiveRecordReferenceOne()
+{
+    return 'id_active_record_reference_one';
+
+} // getIdDivElementActiveRecordReferenceOne
+
+// Hides the active reference one <div>
+function hideActiveRecordReferenceOne()
+{
+    var el_active_reference_one = getDivElementActiveRecordReferenceOne();
+
+    el_active_reference_one.style.display = 'none';
+
+} // hideActiveRecordReferenceOne
+
+// Shows the active reference one <div>
+function displayActiveRecordReferenceOne()
+{
+    var el_active_reference_one = getDivElementActiveRecordReferenceOne();
+
+    el_active_reference_one.style.display = 'block';
+
+} // displayActiveRecordReferenceOne
+
+// Returns the active record reference one URL <div> element
+function getDivElementActiveRecordReferenceOneUrl()
+{
+    return document.getElementById(getIdDivElementActiveRecordReferenceOneUrl());
+
+} // getDivElementActiveRecordReferenceOneUrl
+
+//Returns the identity of the active record reference one <div> element
+function getIdDivElementActiveRecordReferenceOneUrl()
+{
+    return 'id_active_record_reference_one_url';
+
+} // getIdDivElementActiveRecordReferenceOneUrl
+
+// Returns the active record reference one description <div> element
+function getDivElementActiveRecordReferenceOneDescription()
+{
+    return document.getElementById(getIdDivElementActiveRecordReferenceOneDescription());
+
+} // getDivElementActiveRecordReferenceOneDescription
+
+//Returns the identity of the active record reference one <div> element
+function getIdDivElementActiveRecordReferenceOneDescription()
+{
+    return 'id_active_record_reference_one_description';
+
+} // getIdDivElementActiveRecordReferenceOneDescription
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// End Element Functions ///////////////////////////////////////////
